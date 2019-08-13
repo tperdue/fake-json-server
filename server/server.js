fe2869adba171c42558d6522149f12c57baeb3c0
Timothy Perdue <tp.webdev@gmail.com>
2019-08-13 19:41:40 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+app.get('/', async(req, res) => {
+    res.send('Application is working');
+});
+
+
+app.listen(port, ()=> {
+    console.log('Application is running');
+})
