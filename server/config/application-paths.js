cdd604f5e27dc57432f1a7832d4c3e34453e1d98
Timothy Perdue <tp.webdev@gmail.com>
2019-08-13 20:21:01 -0400

    Adding application paths configuration file

---
+++ server/config/application-paths.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+// base paths 
+const applicatonRoot = path.join(__dirname, '..');
+const clientRoot = path.join(__dirname, '..', '..', 'client');
+
+
+// server paths
+
+exports.controllersPath = path.join(applicatonRoot, 'controllers');
+exports.modelsPath = path.join(applicatonRoot, 'models');
+exports.routesPath = path.join(applicatonRoot, 'routes');
+exports.viewsPath = path.join(applicationRoot, 'views');
+
+
+
+// client paths
+
+exports.cssPath = path.join(clientRoot, 'css');
+exports.jsPath = path.join(clientRoot, 'js');
+exports.imgPath = path.join(clientRoot, 'img');
