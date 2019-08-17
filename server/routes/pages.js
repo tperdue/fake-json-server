f5e6314cd5a0d8b030728a51f9ea3f9e9a7d2969
Timothy Perdue <tp.webdev@gmail.com>
2019-08-17 17:10:31 -0400

    Implementing modular controllers

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const pagesController = require('../controllers/pages');
 
 
-router.get('/', async(req, res) => {
-    res.render('home', {});
-});
+router.get('/', pagesController.home);
 
 module.exports = router;
